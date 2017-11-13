8ecc63545c281cf8dc52720ef64585ac9020c7a8
seijoudev <alejo.1395@hotmail.com>
2017-11-13 01:18:08 -0500

    commit2

---
+++ BusinessObjects/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+        public string Correo { get; set; }
+        public string CiudadResidencia { get; set; }
+        public string FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+
+        public Persona() { }
+
+        public Persona(string Nombre, string Apellido, string Cedula, string Correo, string CiudadResidencia, string FechaNacimiento, string Telefono, string Usuario, string Clave)
+        {
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.Cedula = Cedula;
+            this.Correo = Correo;
+            this.CiudadResidencia = CiudadResidencia;
+            this.FechaNacimiento = FechaNacimiento;
+            this.Telefono = Telefono;
+            this.Usuario = Usuario;
+            this.Clave = Clave;
+        }
+    }
+}
---
+++ BusinessObjects/Ubicacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Ubicacion
+    {
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+        public string Direccion { get; set; }
+    }
+}
